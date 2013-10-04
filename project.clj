9f9a61d500240d9d837cacb969bf6ad31244b002
Ryan Lower <rpjlower@gmail.com>
2013-10-03 20:44:13 -0700

    Use HTTP Kit for web server

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
                  [ring/ring-jetty-adapter "1.1.0"]
                  [ring/ring-devel "1.1.0"]
                  [environ "0.2.1"]
-                 [com.taoensso/timbre "2.6.2"]]
+                 [com.taoensso/timbre "2.6.2"]
+                 [http-kit "2.1.10"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.2.1"]]
   :hooks [environ.leiningen.hooks]
--- src/clj_drain/web.clj
+++ src/clj_drain/web.clj
@@ -1,40 +1,17 @@
 (ns clj_drain.web
-  (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
-            [compojure.handler :refer [site]]
-            [compojure.route :as route]
-            [clojure.java.io :as io]
-            [ring.middleware.stacktrace :as trace]
-            [ring.middleware.session :as session]
-            [ring.middleware.session.cookie :as cookie]
-            [ring.adapter.jetty :as jetty]
-            [environ.core :refer [env]]
-            [taoensso.timbre :as timbre :refer (trace debug info warn error fatal spy with-log-level)]))
+  (:use [environ.core :refer [env]]
+        [taoensso.timbre :as timbre :refer (trace debug info warn error fatal spy with-log-level)]
+        [org.httpkit.server]
+        [compojure.core :only [defroutes GET POST DELETE ANY context]]
+        [compojure.route :only [not-found] :as route]
+        [compojure.handler :only [site]]))
 
-(defroutes app
+(defroutes all-routes
   (POST "/drain" {body :body params :params}
-    (info [body params])
+    (info body)
+    (info params)
     {:status 200})
   (route/not-found "Page not found"))
 
-(defn wrap-error-page [handler]
-  (fn [req]
-    (try (handler req)
-         (catch Exception e
-           {:status 500
-            :headers {"Content-Type" "text/html"}
-            :body (slurp (io/resource "500.html"))}))))
-
 (defn -main [& [port]]
-  (let [port (Integer. (or port (env :port) 5000))
-        ;; TODO: heroku config:add SESSION_SECRET=$RANDOM_16_CHARS
-        store (cookie/cookie-store {:key (env :session-secret)})]
-    (jetty/run-jetty (-> #'app
-                         ((if (env :production)
-                            wrap-error-page
-                            trace/wrap-stacktrace))
-                         (site {:session {:store store}}))
-                     {:port port :join? false})))
-
-;; For interactive development:
-;; (.stop server)
-;; (def server (-main))
+  (run-server (site #'all-routes) {:port 5000}))
